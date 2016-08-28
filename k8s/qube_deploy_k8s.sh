2fe8cf374d04b096a2529dee356b283f6f963f9a
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-08-27 18:19:55 -0700

    deploy script

---
+++ k8s/qube_deploy_k8s.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}/..
+
+imageTag=$1
+deploymentNS=$2
+appName=$3
+deploymentName=$4
+deploymentArtifactsFolder=$5
+deploymentTemplate=$6
+
+mkdir -p $deploymentArtifactsFolder
+
+cp -R ${deploymentTemplate}/ ${deploymentArtifactsFolder}
+sed -i.bak 's#%gcr.io/image:version%#${imageTag}#' ${deploymentArtifactsFolder}/*.yaml
+sed -i.bak 's#%namespace%#${deploymentNS}#' ${deploymentArtifactsFolder}/*.yaml
+sed -i.bak 's#%appName%#${appName}#' ${deploymentArtifactsFolder}/*.yaml
+sed -i.bak 's#%deploymentName%#${deploymentName}#' ${deploymentArtifactsFolder}/*.yaml
