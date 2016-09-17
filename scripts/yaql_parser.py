513067a94918772a8346fdc5b847251fcb44e607
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-09-17 08:47:27 -0700

    test

--- scripts/yaql_parser.py
+++ scripts/yaql_parser.py
@@ -1,1 +1,40 @@
+#./yaql-parser.py --file test.yaml --expression "$.bake.destinations.len()"
 #!/usr/bin/python
+import yaql
+import yaml
+import getopt
+import sys
+
+def parseYAQL(file, exp):
+    data_source = yaml.load(open(file, 'r'))
+    engine = yaql.factory.YaqlFactory().create()
+    expression = engine(exp)
+    print file + ":" + exp
+    print expression.evaluate(data=data_source)
+    #print file + ":" + expression
+
+
+def main(argv):
+    file=""
+    expression=""
+    try:
+        opts, args = getopt.getopt(argv, "hfe:d", ["help", "file=","expression="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt == '-d':
+            global _debug
+            _debug = 1
+        elif opt in ("-f", "--file"):
+            file = arg
+        elif opt in ("-e", "--expression"):
+            expression=arg
+
+    parseYAQL(file, expression)    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
