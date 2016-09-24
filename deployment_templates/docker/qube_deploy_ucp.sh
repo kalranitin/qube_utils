f33a9ccd959ecef59eb160fe6e38dd78de69b59f
Hyunji Kim <Hyunji.Kim@ca.com>
2016-09-23 21:19:32 -0700

    qube_deploy_ucp.sh

---
+++ deployment_templates/docker/qube_deploy_ucp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -x
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}/
+
+imageTag=$1
+appName=$2
+endpointName=$3
+endpointDomain=$4
+swarmHost=$5
+deploymentArtifactsFolder=$6
+deploymentTemplate=$7
+
+mkdir -p $deploymentArtifactsFolder
+
+cp -R ${deploymentTemplate}/* ${deploymentArtifactsFolder}
+pwd
+tree ${deploymentTemplate}/
+tree ${deploymentArtifactsFolder}
+sed -i.bak "s#%dtr/image:version%#${imageTag}#" ${deploymentArtifactsFolder}/*.yml
+sed -i.bak "s#%svcname%#${appName}#" ${deploymentArtifactsFolder}/*.yaml
+sed -i.bak "s#%endpoint_name%#${endpointName}#" ${deploymentArtifactsFolder}/*.yml
+sed -i.bak "s#%endpoint_domain%#${endpointDomain}#" ${deploymentArtifactsFolder}/*.yml
+
+export SWARM_HOST=${swarmHost}
+
+docker-compose up -d
