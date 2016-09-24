b454d705223ff01fc0280d9e53359bb4f269daa5
Hyunji Kim <Hyunji.Kim@ca.com>
2016-09-23 21:33:22 -0700

    yaml -> iml

--- deployment_templates/docker/qube_deploy_ucp.sh
+++ deployment_templates/docker/qube_deploy_ucp.sh
@@ -18,7 +18,7 @@ pwd
 tree ${deploymentTemplate}/
 tree ${deploymentArtifactsFolder}
 sed -i.bak "s#%dtr/image:version%#${imageTag}#" ${deploymentArtifactsFolder}/*.yml
-sed -i.bak "s#%svcname%#${appName}#" ${deploymentArtifactsFolder}/*.yaml
+sed -i.bak "s#%svcname%#${appName}#" ${deploymentArtifactsFolder}/*.yml
 sed -i.bak "s#%endpoint_name%#${endpointName}#" ${deploymentArtifactsFolder}/*.yml
 sed -i.bak "s#%endpoint_domain%#${endpointDomain}#" ${deploymentArtifactsFolder}/*.yml
 
