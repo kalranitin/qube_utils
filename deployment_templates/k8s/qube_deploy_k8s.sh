ce6d9c59bafffaa8cc383df697c23ac76114865a
nidve01 <veerendrakumar.niddapu@ca.com>
2016-11-10 16:10:45 -0800

    qubeship api template

--- deployment_templates/k8s/qube_deploy_k8s.sh
+++ deployment_templates/k8s/qube_deploy_k8s.sh
@@ -9,6 +9,7 @@ appName=$3
 deploymentName=$4
 deploymentArtifactsFolder=$5
 deploymentTemplate=$6
+workspace=$7
 
 mkdir -p $deploymentArtifactsFolder
 
@@ -21,6 +22,13 @@ sed -i.bak "s#%namespace%#${deploymentNS}#" ${deploymentArtifactsFolder}/*.yaml
 sed -i.bak "s#%appName%#${appName}#" ${deploymentArtifactsFolder}/*.yaml
 sed -i.bak "s#%deploymentName%#${deploymentName}#" ${deploymentArtifactsFolder}/*.yaml
 
+if [ $deploymentTemplate == "qube_qubeship_apis" ]; then
+cp workspace/qube.yaml .
+spiff merge ${deploymentArtifactsFolder}/merge_template.yaml qube.yaml > result_env.yaml
+spruce merge --prune environment_variables result_env.yaml ${deploymentArtifactsFolder}/kube-nonservice-resources.template.yaml > ${deploymentArtifactsFolder}/kube-nonservice-resources.yaml
+fi
+sleep 300
 kubectl version
 kubectl --namespace=${deploymentNS} apply -f ${deploymentArtifactsFolder}/kube-nonservice-resources.yaml --record
 kubectl --namespace=${deploymentNS} apply -f ${deploymentArtifactsFolder}/kube-service-resources.yaml --record
+
