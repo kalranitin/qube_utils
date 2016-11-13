457437fab70832c7c4f58abdc5eefe1e6f169ab1
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-11-13 12:36:57 -0800

    test

--- deployment_templates/k8s/README.md
+++ deployment_templates/k8s/README.md
@@ -2,4 +2,6 @@ k8s
 	app_template_id
 		kube-nonservice-resources
 		kube-service-resources
-		
+
+
+./qube_deploy_k8s.sh gcr.io/qubeship/qube-swagger-registry:latest production qube-swagger-registry qube-swagger-registry-deployment /tmp/deployment/deploymentTemplate-prod-production-qube-swagger-registry-deployment-3 qube_external_app_v1 ~/Dev/git/qubeship/qube-api-registry prod k8s1 qubeship k8s
--- deployment_templates/k8s/qube_deploy_k8s.sh
+++ deployment_templates/k8s/qube_deploy_k8s.sh
@@ -31,12 +31,12 @@ sed -i.bak "s#%tenant%#${tenant}#" ${deploymentArtifactsFolder}/*.yaml
 sed -i.bak "s#%envProvider%#${provider}#" ${deploymentArtifactsFolder}/*.yaml
 
 if [ $deploymentTemplate == "qube_qubeship_apis" ] || [ $deploymentTemplate == "qube_external_app_v1" ] ; then
-cp $workspace/qube.yaml .
-spiff merge ${deploymentArtifactsFolder}/env_merge_template.yaml qube.yaml > result_env.yaml
-spruce merge --prune ports --prune service_type --prune environment_variables result_env.yaml ${deploymentArtifactsFolder}/kube-nonservice-resources.template.yaml > ${deploymentArtifactsFolder}/kube-nonservice-resources.yaml
-spruce merge --prune ports --prune service_type --prune environment_variables result_env.yaml ${deploymentArtifactsFolder}/kube-service-resources.template.yaml > ${deploymentArtifactsFolder}/kube-service-resources.yaml
+cp $workspace/qube.yaml ${deploymentArtifactsFolder}/
+spiff merge ${deploymentArtifactsFolder}/env_merge_template.yaml ${deploymentArtifactsFolder}/qube.yaml > ${deploymentArtifactsFolder}/result_env.yaml
+sed -ibak 's#- {}$##g' ${deploymentArtifactsFolder}/result_env.yaml
+spruce merge --prune meta --prune ports --prune service_type --prune environment_variables ${deploymentArtifactsFolder}/result_env.yaml ${deploymentArtifactsFolder}/kube-nonservice-resources.template.yaml > ${deploymentArtifactsFolder}/kube-nonservice-resources.yaml
+spruce merge --prune meta --prune ports --prune service_type --prune environment_variables ${deploymentArtifactsFolder}/result_env.yaml ${deploymentArtifactsFolder}/kube-service-resources.template.yaml > ${deploymentArtifactsFolder}/kube-service-resources.yaml
 fi
-
 kubectl version
 kubectl --namespace=${deploymentNS} apply -f ${deploymentArtifactsFolder}/kube-nonservice-resources.yaml --record
 kubectl --namespace=${deploymentNS} apply -f ${deploymentArtifactsFolder}/kube-service-resources.yaml --record
